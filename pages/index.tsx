e14c38675b96de3485b8720f30bad02187c4de56
Igor Mircic <igor.mircic@live.com>
2022-03-28 00:40:34 +0200

    ref(hobbies): add object destructuringg to make code a bit cleaner :)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,28 @@
 // general
-import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 // components
-import PersonInfo from "../components/PersonInfo";
-import ExperiencesList from "../components/ExperiencesList";
-import HobbiesList from "../components/HobbiesList";
+import PersonInfo from '../components/PersonInfo';
+import ExperiencesList from '../components/ExperiencesList';
+import HobbiesList from '../components/HobbiesList';
 // data
-import { personData } from "../consts/personData";
+import { personData } from '../consts/personData';
 
 const Home: NextPage = () => {
+  const { personInfo, experiences, hobbies } = personData;
   return (
     <div className={styles.container}>
       <Head>
         <title>{personData.personInfo.name}&apos;s resume</title>
-        <meta name="description" content="Resume webpage" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Resume webpage' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <PersonInfo {...personData.personInfo}></PersonInfo>
-        <ExperiencesList experiences={personData.experiences}></ExperiencesList>
-        <HobbiesList hobbies={personData.hobbies}></HobbiesList>
+        <PersonInfo {...personInfo}></PersonInfo>
+        <ExperiencesList {...{ experiences }}></ExperiencesList>
+        <HobbiesList {...{ hobbies }}></HobbiesList>
       </main>
 
       <footer className={styles.footer}></footer>
