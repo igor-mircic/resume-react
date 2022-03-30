344297d86cf3bdd678b077c93e98382b9f1fe882
Mladen <mladen.stankovic90@hotmail.com>
2022-03-30 23:21:15 +0200

    dev...

--- components/ExperiencesList.tsx
+++ components/ExperiencesList.tsx
@@ -1,16 +1,19 @@
-import ExperienceItem from "./ExperienceItem";
+import ExperienceItem from './ExperienceItem';
 
 const ExperiencesList = (props: IExpProp) => {
-  return (
-    <div>
-      <h1>Experiences</h1>
-      {props.experiences.map(({ ...expItem }) => {
-        return (
-          <ExperienceItem {...expItem} key={expItem.jobTitle}></ExperienceItem>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Experiences</h1>
+            {props.experiences.map(({ ...expItem }) => {
+                return (
+                    <ExperienceItem
+                        {...expItem}
+                        key={expItem.title}
+                    ></ExperienceItem>
+                );
+            })}
+        </div>
+    );
 };
 
 export default ExperiencesList;
